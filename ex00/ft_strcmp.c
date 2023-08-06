cdd7ed04bda0ee854020fe3595fd7ecccd031292
Peijing Teh <pteh@c1r3s2.42singapore.sg>
2023-08-06 10:42:43 +0800

    complete ex01

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,32 +6,75 @@
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 14:41:54 by pteh              #+#    #+#             */
-/*   Updated: 2023/08/03 14:31:34 by pteh             ###   ########.fr       */
+/*   Updated: 2023/08/06 10:22:34 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	pointer_comp(char *string1, char *string2)
-{
-
+/*#include <stdio.h>
+#include <string.h>*/
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	ascii_val;
+	while (*s2 != '\0' && *s1 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+/*
+		int	ascii_val;
 	int	ascii_inc;
 
 	ascii_val = 0;
-	while (*s2 != '\0')
-	{
-		while (*s1 != '\0')
-		{
-		if (*s1 == *s2)
+if (*s1 == *s2)
 			ascii_inc = 0;
 		else
 			ascii_inc = *s1 - *s2;
 		ascii_val = ascii_val + ascii_inc;
-		s1++;
-		}
-		s2++;
-	}
-	return (ascii_val);
+		
+
+
+int	main(void)
+{
+	char	*string1;
+	char	*string2;
+	char	*string3;
+	char	*string4;
+	char	*string5;
+
+	string1 = "ABC";
+	string2 = "AB";
+	string3 = "ABA";
+	string4 = "ABJ";
+	string5 = "ABZ";
+
+	printf("original:%s\n", string1);
+	printf("original:%s\n", string2);
+	printf("original:%s\n", string3);
+	printf("original:%s\n", string4);
+	printf("original:%s\n", string5);
+	
+	int	a = ft_strcmp(string1, string1);
+	int	b = ft_strcmp(string1, string2);
+	int	c = ft_strcmp(string3, string5);
+	int	d = ft_strcmp(string4, string1);
+	
+	int	f = strcmp(string1, string1);
+	int	g = strcmp(string1, string2);
+	int	h = strcmp(string3, string5);
+	int	i = strcmp(string4, string1);
+	
+	printf("ft_strcmp ABC ABC:%d\n", a);
+	printf("ft_strcmp ABC AB:%d\n", b);
+	printf("ft_strcmp ABA ABZ:%d\n", c);
+	printf("ft_strcmp ABJ ABC:%d\n", d);
+	printf("strcmp:%d\n", f);
+	printf("strcmp:%d\n", g);
+	printf("strcmp:%d\n", h);
+	printf("strcmp:%d\n", i);
+
+
 }
+*/
---
+++ ex01/ft_strncmp.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/06 10:19:41 by pteh              #+#    #+#             */
+/*   Updated: 2023/08/06 10:42:01 by pteh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+*/
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 1;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+/*
+int	main(void)
+{
+	char	*string1;
+	char	*string2;
+	char	*string3;
+	char	*string4;
+	char	*string5;
+	unsigned int	i;
+	unsigned int	j;
+
+	string1 = "ABC";
+	string2 = "AB";
+	string3 = "ABA";
+	string4 = "ABJ";
+	string5 = "ABZ";
+	i = 3;
+	j = 3;
+
+	printf("original:%s\n", string1);
+	printf("original:%s\n", string2);
+	printf("original:%s\n", string3);
+	printf("original:%s\n", string4);
+	printf("original:%s\n", string5);
+	
+	int	a = ft_strncmp(string1, string1,i);
+	int	b = ft_strncmp(string1, string2,i);
+	int	c = ft_strncmp(string3, string5,j);
+	int	d = ft_strncmp(string4, string1,i);
+	
+	int	f = strncmp(string1, string1,i);
+	int	g = strncmp(string1, string2,i);
+	int	h = strncmp(string3, string5,j);
+	int	l = strncmp(string4, string1,i);
+	
+	printf("ft_strncmp ABC ABC:%d\n", a);
+	printf("ft_strncmp ABC AB:%d\n", b);
+	printf("ft_strncmp ABA ABZ:%d\n", c);
+	printf("ft_strncmp ABJ ABC:%d\n", d);
+	printf("strncmp:%d\n", f);
+	printf("strncmp:%d\n", g);
+	printf("strncmp:%d\n", h);
+	printf("strncmp:%d\n", l);
+}
+*/
